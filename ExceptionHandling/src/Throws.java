c138deec8d43adec5273db552faed67d926b7c41
boo <boo@192.168.1.3>
2020-04-02 19:48:38 +0530

    commit

--- ExceptionHandling/src/Throws.java
+++ ExceptionHandling/src/Throws.java
@@ -3,7 +3,7 @@ public class Throws {
 	public static void main(String[] args) throws InterruptedException{
 		
 		Thread.sleep(1000);
-		System.out.println("hello");
+		System.out.println("hellocdomo");
 		
 		
 	}
